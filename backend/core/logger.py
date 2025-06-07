d6c26ab22faa0d3493f76eb8dbb5aff43256533a
Krystian Fryca <kp15731@gmail.com>
2025-06-07 22:23:53 +0200

    [feat/logger_fix] - logger fix

--- backend/core/logger.py
+++ backend/core/logger.py
@@ -1,13 +1,17 @@
 import logging
+import os
 
-# Logger config
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+# Logger setup
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
         logging.StreamHandler(),
-        logging.FileHandler("logs/detected_issues.log")
-        ]
-    )
+        logging.FileHandler(os.path.join(LOG_DIR, "detected_issues.log"))
+    ]
+)
 
 logger = logging.getLogger("log-analyzer")
